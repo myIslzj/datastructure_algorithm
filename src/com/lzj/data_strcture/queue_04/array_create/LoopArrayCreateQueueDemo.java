5ab165646e42c1bc59a57ae446512894b91923e8
LiZhuJun <lzj960798100@163.com>
2023-05-23 14:22:30 +0800

    组实现环形队列(实现方式一：取莫)

--- src/com/lzj/data_strcture/queue_04/array_create/LoopArrayCreateQueueDemo.java
+++ src/com/lzj/data_strcture/queue_04/array_create/LoopArrayCreateQueueDemo.java
@@ -11,26 +11,26 @@ import java.util.Scanner;
  * （3）队列满时：(rear + 1) % maxSize = front
  * （4）队列为空：rear == front
  * （5）队列有效数据个数：(rear + maxSize - front) % maxSize
- * 缺点：rear不断增大，存在数值越界风险，不能将队列添满
+ * 缺点：不能将队列添满
  * 实现方式二：front、rear比对法
  * （1）不需要设置辅助位
  * （2）初始化时：front = 0, rear = 0
  * （3）isLoop 记录是否已形成环
  * （4）队列为空时：false == isLoop && front == rear
- * （5）可添加元素时： front < rear
- * （6）front、rear等于maxSize后重置为0
- * （7）队列已满时：isLoop == false && rear - front == maxSize 或 isLoop == true && front == rear
+ * （5）front、rear等于maxSize后重置为0
+ * （6）队列已满时：isLoop == true && front == rear
+ * （7）有效个数：若 isLoop == true，则有 maxSize - (front - rear)、若 isLoop == false，则有 rear - front
  */
 public class LoopArrayCreateQueueDemo {
     public static void main(String[] args) {
 
         // 实现方式一
-//        LoopArrayCreateQueue_1 queue = new LoopArrayCreateQueue_1(5);
+        LoopArrayCreateQueue_1 queue = new LoopArrayCreateQueue_1(5);
         // 实现方式二
-        LoopArrayCreateQueue_2 queue = new LoopArrayCreateQueue_2(5);
+//        LoopArrayCreateQueue_2 queue = new LoopArrayCreateQueue_2(5);
         Scanner s = new Scanner(System.in);
         while (true) {
-            System.out.println("请输入操作，1：入队、2：出队");
+            System.out.println("请输入操作，1：入队、2：出队、3：打印");
             int i = s.nextInt();
             switch (i) {
                 case 1:
@@ -39,6 +39,9 @@ public class LoopArrayCreateQueueDemo {
                     break;
                 case 2:
                     queue.deq();
+                    break;
+                case 3:
+                    queue.show();
                     break;
                 default:
                     break;
@@ -63,8 +66,8 @@ class LoopArrayCreateQueue_1 {
             System.out.println("最大长度无效。。。。");
             return;
         }
-        this.maxSize = maxSize;
-        arr = new int[this.maxSize + 1];
+        this.maxSize = maxSize + 1;
+        arr = new int[this.maxSize];
     }
 
     /**
@@ -80,7 +83,7 @@ class LoopArrayCreateQueue_1 {
             return false;
         }
         arr[rear] = e;  // 元素入队
-        rear++;  // 入队下标后移，准备下一个入队元素的位置
+        rear = (rear + 1) % maxSize;  // 入队下标后移，准备下一个入队元素的位置
         return true;
     }
 
@@ -91,17 +94,36 @@ class LoopArrayCreateQueue_1 {
      */
     public boolean deq() {
         // 判断是否有可出队元素
-        if (front == -1 || front == maxSize) {
+        if (size() < 1) {
             System.out.println("队列无可出队元素。。。。");
             return false;
         }
         System.out.println(arr[front] + "出队");
-        front++;
+        front = (front + 1) % maxSize;
         return true;
+    }
+
+    /**
+     * 获取长度
+     *
+     * @return
+     */
+    public int size() {
+        return (maxSize + rear - front) % maxSize;
+    }
+
+    /**
+     * 遍历
+     */
+    public void show() {
+        for (int i = front; i < front + size(); i++) {
+            System.out.println(arr[i % maxSize]);
+        }
     }
 
 
 }
+
 /**
  * 实现方式二
  */
@@ -129,7 +151,7 @@ class LoopArrayCreateQueue_2 {
      * @return
      */
     public boolean enq(int e) {
-        if (rear == maxSize){  // 已添加到末尾
+        if (rear == maxSize) {  // 已添加到末尾
             rear = 0;  // 重置入队位为起始位置，形成环
             isLoop = true;  // 标志已形成环
         }
@@ -155,11 +177,29 @@ class LoopArrayCreateQueue_2 {
             return false;
         }
         System.out.println(arr[front] + "出队");
-        if (++front == maxSize){  // 已读取到末尾
+        if (++front == maxSize) {  // 已读取到末尾
             front = 0;  // 重置出队位为起始位置，形成环
             isLoop = false;  // 标志环解除
         }
         return true;
+    }
+
+    /**
+     * 获取长度
+     *
+     * @return
+     */
+    public int size() {
+        return isLoop ? maxSize - (front - rear) : rear - front;
+    }
+
+    /**
+     * 遍历
+     */
+    public void show() {
+        for (int i = front; i < front + size(); i++) {
+            System.out.println(arr[i % maxSize]);
+        }
     }
 
 }
